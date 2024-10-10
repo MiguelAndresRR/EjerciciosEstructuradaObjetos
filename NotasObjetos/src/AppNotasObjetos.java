d2812dfa478b4a09228a582cbe3f5fadc7005428
macrolive578 <macrolive578@gmail.com>
2024-10-10 17:18:33 -0500

    Estructurado2.0

--- NotasObjetos/src/AppNotasObjetos.java
+++ NotasObjetos/src/AppNotasObjetos.java
@@ -3,10 +3,10 @@ import clases.*;
 
 public class AppNotasObjetos {
     public static void main(String[] args) throws Exception {
-           Estudiante est = new Estudiante(null,Float.parseFloat("2.5"),Float.parseFloat("5.0"));
-           Estudiante est1 = new Estudiante(null,Float.parseFloat("2.5"),Float.parseFloat("5.0"));
-           est.notaFinal();
-           est1.notaFinal();
-           System.out.println("El estudiante "+ est.obtenerNombre()+" obtuvo una nota final de "est.obtenerNotaFinal()+ " por Consiguiente "+ est.obtenerMensaje());
+           Estudiante est = new Estudiante("Carlos Rodriguez",Float.parseFloat("2.5"),Float.parseFloat("5.0"));
+           Estudiante est1 = new Estudiante("Santiago ibañez",Float.parseFloat("2.5"),Float.parseFloat("5.0"));
+           est.calcularNotaFinal();
+           est1.calcularNotaFinal();
+           System.out.println("El estudiante "+ est.nombre()+" obtuvo una nota final de "+est.notafinal()+ " por Consiguiente "+ est.obtenerMensaje());
     }
 }
--- NotasObjetos/src/clases/Estudiante.java
+++ NotasObjetos/src/clases/Estudiante.java
@@ -1,53 +1,59 @@
 package clases;
 
 public class Estudiante {
-
     private String nombre;
     private float nota1;
     private float nota2;
     private float notafinal;
-
-    public Estudiante(String nombre){
-        this.nombre = nombre;
-    }
     
-    public Estudiante (String nombre, float nota1, float nota2) {
+    public Estudiante(String nombre) {
+        this.nombre = nombre;  
+    }
+
+    public Estudiante(String nombre, float nota1, float nota2) {
         this.nombre = nombre;
         this.nota1 = nota1;
         this.nota2 = nota2;
     }
-    public void asignarNombre(String nombre){
+
+    public void asignarNombre(String nombre) {
         this.nombre = nombre;
     }
-    public void obtenerNombre(){
-        return;
-    }
-    public void asignarNotaParcial1(Float nota1){
+
+    public void asignarNotaParcial1(float nota1) {
         this.nota1 = nota1;
     }
-    public void asignarNotaParcial2(Float nota2){
+
+    public void asignarNotaParcial2(float nota1) {
         this.nota2 = nota2;
     }
 
-    public void obtenerNotaParcial1(){
-        return;
+    public float obtenerNotaParcial1() {
+        return nota1;
     }
 
-    public void obtenerNotaParcial2(){
-        return; 
-    }
-    
-    public void calcularNotaFinal(){
-        notafinal = (nota1 + nota2)/2;
-    }
-    public void obtenerNotaFinal(){
-        return;
+    public float obtenerNotaParcial2() {
+        return nota2;
     }
 
-    public String obtenerMensaje(){
-        if (notafinal <= 3)
-        return "Reprobo";
-        else
-        return "Aprobado";
+    public String obtenerNombre() {
+        return  nombre;
+    }
+
+    public void calcularNotaFinal() {
+        notafinal = (nota1 + nota2) / 2;
+    }
+
+    public Float obtenerNotaFinal() {
+        return notafinal;
+    }
+
+
+    public String obtenerMensaje() {
+        if (notafinal < 3) {
+            return "reprobó";
+        } else {
+            return "aprobó";
+        }
     }
 }
